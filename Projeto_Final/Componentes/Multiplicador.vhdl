13ea70db9e66484ac515a3c13a8db66f7731f918
Rafael Nóbrega de Lima <61426596+KylixXD@users.noreply.github.com>
2021-04-15 20:35:55 -0400

    Update Multiplicador.vhdl

--- Projeto_Final/Componentes/Multiplicador.vhdl
+++ Projeto_Final/Componentes/Multiplicador.vhdl
@@ -2,17 +2,18 @@ Library ieee;
 Use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-Entity Multiplicador is
-    Port (a,b : in Std_logic_vector (7 downto 0);
-            Produto : Out Std_Logic_Vector (7 downto 0);
+Entity multiplicador is
+	 Port (a,b    : in Std_logic_vector (7 downto 0);
+         Produto : Out Std_Logic_Vector (15 downto 0)
+			);
 
-end Multiplicador; 
-
-Architecture Multiplicador_8b is
+end multiplicador; 
+ 
+Architecture main of multiplicador is
     Signal sig_1, sig_2 : Signed (7 downto 0 );
 Begin
-        sig_1 <- signed(a);
-        sig_2 <- signed(b);
-        Produto <- Std_Logic_Vector(sig_1 * sig_2);
+        sig_1   <= signed(a);
+        sig_2   <= signed(b);
+        Produto <= Std_Logic_Vector(sig_1 * sig_2);
 
-End Multiplicador;
+End main;
